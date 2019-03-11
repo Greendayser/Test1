3a539c181c1a676d7d4706a84262869a4234fcb3
samir.zaarouri <samir.zaarouri@epita.fr>
2019-03-11 15:42:54 +0100

    for celine
    ajout char +plateform

---
+++ Assets/characbehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characbehavior : MonoBehaviour {
+    
+    public RigidBody2D rb;
+    public float vitesse;
+	// Use this for initialization
+	void Start () {
+        rb.velocity += new vector2(vitesse, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
